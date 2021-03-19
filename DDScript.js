a552f653ccf224cbf6d14a9b719bb7a0f0ee3133
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-03-19 21:30:05 +0800

    Create DDScript.js

---
+++ DDScript.js
@@ -0,0 +1,5 @@
+# 叮咚买菜
+cron "10 6 8 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/mcdd/mcdd.js,tag=叮咚买菜
+
+# 叮咚农场
+cron "1 8,12,17 * * *" script-path=https://raw.githubusercontent.com/iepngs/Script/master/dingdong/index.js, tag=叮咚农场
