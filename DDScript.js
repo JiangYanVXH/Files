5097907fa2fb34b204d822ca8c905d21347a5e2f
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-03-20 00:19:22 +0800

    Delete DDScript.js

--- DDScript.js
+++
@@ -1,5 +0,0 @@
-# 叮咚农场
-cron "1 8,12,17 * * *" script-path=https://raw.githubusercontent.com/iepngs/Script/master/dingdong/index.js,tag=叮咚养鱼
-
-# 叮咚买菜
-cron "10 6 8 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/mcdd/mcdd.js,tag=叮咚买菜
