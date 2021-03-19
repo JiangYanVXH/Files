f487a7ac7dbea9ffca3a528846d49110db78d53d
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-03-19 22:44:19 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -1,5 +1,5 @@
 # 叮咚买菜
-cron "1 0 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/mcdd/mcdd.js, tag=叮咚买菜
+# cron "40 10 * * *" script-path=https://raw.githubusercontent.com/chavyleung/scripts/master/mcdd/mcdd.js, enabled=true, tag=叮咚买菜
 
 # 叮咚农场
 cron "1 8,12,17 * * *" script-path=https://raw.githubusercontent.com/iepngs/Script/master/dingdong/index.js,tag=叮咚养鱼
