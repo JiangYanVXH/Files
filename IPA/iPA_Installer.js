1432d869001c100a1d5a142dc911af24cd11ac23
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-03-29 13:58:02 +0800

    Add files via upload

---
+++ IPA/iPA_Installer.js
@@ -0,0 +1,141 @@
+
+/*
+源文件
+https://raw.githubusercontent.com/langkhach270389/iPA_Plist/master/Jsbox.plist
+/*
+
+var port_number = 8080
+var plist_url = "itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/JiangYanVXH/Files/main/IPA/Esign.plist"
+
+$app.strings = {
+  "en": {
+    "starterror": "Not support running in this way",
+    "ftypeerror": " is not ipa file",
+    "installtitle": "Installing...",
+    "installmsg": "\n\nYou can check on Homescreen.\nPlease tap \"Done\" button after finished",
+    "inerrtitle": "IPA file import error",
+    "inerrmsg": "Please rerun the script"
+  },
+}
+
+if ($app.env == $env.app) {
+  $drive.open({
+    handler: function(data) {
+      fileCheck(data)
+    }
+  })
+}
+// 从 Action Entension 启动
+else if ($app.env == $env.action) {
+  fileCheck($context.data)
+}
+
+else {
+  $ui.error($l10n("starterror"))
+  delayClose(2)
+}
+
+
+function startServer(port) {
+  $http.startServer({
+    port: port,
+    path: "",
+    handler: function(result) {
+      console.info(result.url)
+    }
+  })
+}
+
+function fileCheck(data) {
+  if (data && data.fileName) {
+    var fileName = data.fileName;
+    if (fileName.indexOf(".ipa") == -1) {
+      $ui.error(fileName + $l10n("ftypeerror"))
+      delayClose(2)
+    } else {
+      install(fileName, data);
+    }
+  }
+}
+
+function install(fileName, file) {
+  var result = $file.write({
+    data: file,
+    path: "app.ipa"
+  })
+  if (result) {
+    startServer(port_number)
+    $location.startUpdates({
+      handler: function(resp) {
+        console.info(resp.lat + " " + resp.lng + " " + resp.alt)
+      }
+    })
+    var preResult = $app.openURL(plist_url);
+    if (preResult) {
+      $ui.alert({
+        title: $l10n("installtitle"),
+        message: "\n" + fileName + $l10n("installmsg"),
+        actions: [{
+          title: "Cancel",
+          style: "Cancel",
+          handler: function() {
+            $http.stopServer()
+            $file.delete("app.ipa")
+            delayClose(0.2)
+          }
+        },
+        {
+          title: "Done",
+          handler: function() {
+            $http.stopServer()
+            $file.delete("app.ipa")
+            delayClose(0.2)
+          }
+        }]
+      })
+    } else {
+      $ui.alert({
+        title: "Open itms-services scheme failed",
+        message: "Please contact the author @axel_burks",
+        actions: [{
+          title: "Cancel",
+          style: "Cancel",
+          handler: function() {
+            delayClose(0.2)
+          }
+        },
+        {
+          title: "OK",
+          handler: function() {
+            $app.openURL("tg://resolve?domain=axel_burks")
+          }
+        }]
+      })
+    }
+  } else {
+    $ui.alert({
+      title: $l10n("inerrtitle"),
+      message: $l10n("inerrmsg"),
+      actions: [{
+        title: "OK",
+        style: "Cancel",
+        handler: function() {
+          delayClose(0.2)
+        }
+      }]
+    })
+  }
+}
+
+function delayClose(time) {
+    $location.stopUpdates()
+    $thread.main({
+      delay: time,
+      handler: function() {
+        if ($app.env == $env.action || $app.env == $env.safari) {
+          $context.close()
+        }
+        $app.close()
+      }
+    })
+}
