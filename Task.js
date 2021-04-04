01a193a581213106ec7584cb07300ff3cbcf3a04
JiangYanVXH <78822111+JiangYanVXH@users.noreply.github.com>
2021-04-04 10:45:14 +0800

    Update Task.js

--- Task.js
+++ Task.js
@@ -106,7 +106,7 @@ cron "42 1-23/4 * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jd
 
 # 宠汪汪积分兑换奖品
 # cron "1,5 0-16/8 * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_joy_reward.js,tag=宠汪汪积分兑换奖品
-cron "1,5 0-16/8 * * *" script-path=jd_joy_reward.js,tag=宠汪汪积分兑换奖品
+cron "1,5 0-16/8 * * *" script-path=/jd_joy_reward.js,tag=宠汪汪积分兑换奖品
 
 # 取关京东店铺和商品
 cron "50,52 23 * * *" script-path=https://jdsharedresourcescdn.azureedge.net/jdresource/jd_unsubscribe.js,tag=取关京东店铺商品
